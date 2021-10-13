465ee4afcb0ef89155fb9adc1b1e1632d03c05ff
Bazyli Cyran <bazyli.cyran@gmail.com>
2021-10-13 21:22:23 +0200

    Improve / cleanup polybar redshift script

--- polybar/.config/polybar/modules/redshift.sh
+++ polybar/.config/polybar/modules/redshift.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-pid=$(pgrep -x redshift)
+pid="$(pgrep -x redshift)"
+readonly pid
 
+icon_on=''
+icon_off=''
 hide_day='false'
 
 redshift_print() {
     local output status temp period
-    if [ "$pid" ]; then
+
+    if [[ -n "$pid" ]]; then
         status="$(LC_ALL=C redshift -p 2> /dev/null)"
         temp="$(echo "$status" | grep -oP '(?<=Color temperature: )\w+')"
         period="$(echo "$status" | grep -oP '(?<=Period: )\w+')"
@@ -20,18 +24,18 @@ redshift_print() {
         output="$icon_off off"
     fi
 
-    echo "$output"
+    echo "${output# }"
 }
 
 redshift_toggle() {
-    if [ "$pid" ]; then
+    if [[ -n "$pid" ]]; then
         killall -e redshift
     else
         redshift > /dev/null 2>&1 &
     fi
 }
 
-while [ "$#" -gt 0 ]; do
+while [[ "$#" -gt 0 ]]; do
     case "$1" in
         --icon-on)
             shift
@@ -50,6 +54,7 @@ while [ "$#" -gt 0 ]; do
     esac
     shift
 done
+readonly icon_on icon_off hide_day
 
 case "$1" in
     toggle)
