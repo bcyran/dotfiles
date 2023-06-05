8072f438f85e895b3cecc2e0308d21306dfd6f05
Bazyli Cyran <bazyli.cyran@gmail.com>
2023-06-05 19:18:11 +0200

    system: Start Hyprland on tty1 by default

--- home/private_dot_config/private_fish/config.fish
+++ home/private_dot_config/private_fish/config.fish
@@ -17,8 +17,8 @@ set -gx tide_right_prompt_items status cmd_duration context jobs node virtual_en
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty &> /dev/null
-    else if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
         exec Hyprland &> /dev/null
+    else if test -z "$DISPLAY" -a "$XDG_VTNR" = 2
+        exec startx -- -keeptty &> /dev/null
     end
 end
