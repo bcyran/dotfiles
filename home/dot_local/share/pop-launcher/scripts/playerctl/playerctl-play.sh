3aa01ba3debc0811961335e21819d9623174f16b
Bazyli Cyran <bazyli.cyran@gmail.com>
2024-01-28 18:01:04 +0100

    onagre: Add basic custom action scripts

---
+++ home/dot_local/share/pop-launcher/scripts/playerctl/playerctl-play.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# name: Play
+# icon: media-playback-start
+# description: Start the media player
+# keywords: play player
+
+playerctl play
---
+++ home/dot_local/share/pop-launcher/scripts/playerctl/playerctl-previous.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# name: Previous track
+# icon: media-skip-backward
+# description: Skip to the previous media track
+# keywords: previous player
+
+playerctl previous
