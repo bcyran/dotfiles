4d6eed9ec44d073556f3cdece7692677a3af5a16
Bazyli Cyran <bazyli@cyran.dev>
2024-03-13 20:40:14 +0100

    nvim: Add copilot status indicator and auto trigger binding

--- home/private_dot_config/nvim/lua/plugins/ai.lua
+++ home/private_dot_config/nvim/lua/plugins/ai.lua
@@ -1,8 +1,18 @@
 return {
+  -- Base copilot plugin
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
+    keys = {
+      {
+        "<leader>at",
+        function()
+          require("copilot.suggestion").toggle_auto_trigger()
+        end,
+        desc = "Toggle Copilot auto trigger",
+      },
+    },
     opts = {
       panel = {
         enabled = true,
@@ -45,6 +55,7 @@ return {
     end,
   },
 
+  -- Copilot chat plugin
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
@@ -64,5 +75,43 @@ return {
       "CopilotChatCommit",
       "CopilotChatCommitStaged",
     },
+  },
+
+  -- Copilot indicator in statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local Util = require("lazyvim.util")
+      local colors = {
+        [""] = Util.ui.fg("Special"),
+        ["Normal"] = Util.ui.fg("Special"),
+        ["Warning"] = Util.ui.fg("DiagnosticError"),
+        ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+      }
+      table.insert(opts.sections.lualine_x, 2, {
+        function()
+          local icon = require("lazyvim.config").icons.kinds.Copilot
+          local status = require("copilot.api").status.data
+          return icon .. (status.message or "")
+        end,
+        cond = function()
+          if not package.loaded["copilot"] then
+            return
+          end
+          local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+          if not ok then
+            return false
+          end
+          return ok and #clients > 0
+        end,
+        color = function()
+          if not package.loaded["copilot"] then
+            return
+          end
+          local status = require("copilot.api").status.data
+          return colors[status.status] or colors[""]
+        end,
+      })
+    end,
   },
 }
