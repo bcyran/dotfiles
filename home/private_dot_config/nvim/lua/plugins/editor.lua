bb028245dabeb16607b77d95f58142c647a5e0c7
Bazyli Cyran <bazyli@cyran.dev>
2024-03-13 20:56:22 +0100

    nvim: Add treesj plugin

--- home/private_dot_config/nvim/lua/plugins/editor.lua
+++ home/private_dot_config/nvim/lua/plugins/editor.lua
@@ -8,4 +8,16 @@ return {
       { "<leader>tt", "<cmd>Tableize<cr>", desc = "Tableize data" },
     },
   },
+
+  {
+    "Wansmer/treesj",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    keys = {
+      { "<leader>cj", "<cmd>TSJJoin<cr>", desc = "Join lines" },
+      { "<leader>cs", "<cmd>TSJSplit<cr>", desc = "Split lines" },
+    },
+    opts = {
+      use_default_keymaps = false,
+    },
+  },
 }
