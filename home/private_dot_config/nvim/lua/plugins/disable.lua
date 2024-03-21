3c87947cb0d13ccd38803271a838b6d0f66b435f
Bazyli Cyran <bazyli@cyran.dev>
2024-03-21 18:21:56 +0100

    nvim: Remove snippets from completion

--- home/private_dot_config/nvim/lua/plugins/disable.lua
+++ home/private_dot_config/nvim/lua/plugins/disable.lua
@@ -3,4 +3,8 @@ return {
     "nvimdev/dashboard-nvim",
     enabled = false,
   },
+  {
+    "rafamadriz/friendly-snippets",
+    enable = false,
+  },
 }
--- home/private_dot_config/nvim/lua/plugins/editor.lua
+++ home/private_dot_config/nvim/lua/plugins/editor.lua
@@ -34,4 +34,13 @@ return {
       { "<leader>cu", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle undotree" },
     },
   },
+
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = vim.tbl_filter(function(v)
+        return not vim.tbl_contains({ "luasnip" }, v.name)
+      end, opts.sources)
+    end,
+  },
 }
