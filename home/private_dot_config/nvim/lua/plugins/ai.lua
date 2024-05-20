b8a7a62653ca8d95b2b2a741d08f473c228fb6ef
Bazyli Cyran <bazyli@cyran.dev>
2024-05-20 19:59:10 +0200

    nvim: Codeium setup

--- home/private_dot_config/nvim/lua/plugins/ai.lua
+++ home/private_dot_config/nvim/lua/plugins/ai.lua
@@ -1,7 +1,10 @@
+local settings = require("config.settings")
+
 return {
   -- Base copilot plugin
   {
     "zbirenbaum/copilot.lua",
+    enabled = settings.copilot_enabled,
     cmd = "Copilot",
     event = "InsertEnter",
     keys = {
@@ -38,7 +41,6 @@ return {
         ["*"] = false,
       },
     },
-    enabled = true,
     init = function()
       local cmp_status_ok, cmp = pcall(require, "cmp")
       if cmp_status_ok then
@@ -57,6 +59,7 @@ return {
   -- Copilot chat plugin
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    enable = settings.copilot_enabled,
     branch = "canary",
     dependencies = {
       { "zbirenbaum/copilot.lua" },
@@ -130,41 +133,92 @@ return {
     },
   },
 
-  -- Copilot indicator in statusline
+  -- Codeium (alternative to Copilot)
+  {
+    "Exafunction/codeium.vim",
+    enabled = settings.codeium_enabled,
+    cmd = "Codeium",
+    event = "BufEnter",
+    keys = {
+      {
+        "<leader>at",
+        "<cmd>Codeium Toggle<cr>",
+        desc = "Toggle Codeium auto trigger",
+      },
+    },
+    config = function()
+      vim.g.codeium_disable_bindings = 1
+      vim.g.codeium_filetypes_disabled_by_default = 1
+      vim.g.codeium_filetypes = {
+        rust = true,
+        python = true,
+        bash = true,
+        lua = true,
+      }
+      vim.keymap.set("i", "<C-y>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<C-n>", function()
+        return vim.fn["codeium#CycleOrComplete"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<C-p>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<C-e>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
+  },
+
+  -- Copilot / Codeium indicator in statusline
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
       local Util = require("lazyvim.util")
-      local colors = {
-        [""] = Util.ui.fg("Special"),
-        ["Normal"] = Util.ui.fg("Special"),
-        ["Warning"] = Util.ui.fg("DiagnosticError"),
-        ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
-      }
-      table.insert(opts.sections.lualine_x, 2, {
-        function()
-          local icon = require("lazyvim.config").icons.kinds.Copilot
-          local status = require("copilot.api").status.data
-          return icon .. (status.message or "")
-        end,
-        cond = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
-          if not ok then
-            return false
-          end
-          return ok and #clients > 0
-        end,
-        color = function()
-          if not package.loaded["copilot"] then
-            return
-          end
-          local status = require("copilot.api").status.data
-          return colors[status.status] or colors[""]
-        end,
-      })
+
+      if settings.copilot_enabled then
+        local colors = {
+          [""] = Util.ui.fg("Special"),
+          ["Normal"] = Util.ui.fg("Special"),
+          ["Warning"] = Util.ui.fg("DiagnosticError"),
+          ["InProgress"] = Util.ui.fg("DiagnosticWarn"),
+        }
+        table.insert(opts.sections.lualine_x, 2, {
+          function()
+            local icon = require("lazyvim.config").icons.kinds.Copilot
+            local status = require("copilot.api").status.data
+            return icon .. (status.message or "")
+          end,
+          cond = function()
+            if not package.loaded["copilot"] then
+              return
+            end
+            local ok, clients = pcall(require("lazyvim.util").lsp.get_clients, { name = "copilot", bufnr = 0 })
+            if not ok then
+              return false
+            end
+            return ok and #clients > 0
+          end,
+          color = function()
+            if not package.loaded["copilot"] then
+              return
+            end
+            local status = require("copilot.api").status.data
+            return colors[status.status] or colors[""]
+          end,
+        })
+      end
+
+      if settings.codeium_enabled then
+        table.insert(opts.sections.lualine_x, 2, {
+          function()
+            local icon = require("lazyvim.config").icons.kinds.Codeium
+            local status = vim.api.nvim_call_function("codeium#GetStatusString", {})
+            return icon .. status
+          end,
+          color = Util.ui.fg("Special"),
+        })
+      end
     end,
   },
 }
